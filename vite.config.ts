dcfb47ed7cf2cd1aee89b2b41f31aea7dbdfb84d
Darshit Varshney <studytillexam12@gmail.com>
2026-04-14 14:28:01 +0530

    Add tsconfig paths plugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/", // 🔥 THIS LINE FIXES YOUR ISSUE
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  base: "/",
 });
